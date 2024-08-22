5a0439cc03886dbf6d80c6490cab459d2d0d4aa4
王先生 <1324771795@qq.com>
2024-08-22 23:20:03 +0800

    更新 GeneratorPage 和 ComboBoxEdit 控件
    在 GeneratorPage.xaml 中添加了 enums 命名空间引用，并在 Grid 元素中添加了 Margin 属性。将 TabItem 的 Header 从 "Input" 修改为 "Easy Setting"。在 StackPanel 中添加了一个新的 ComboBoxEdit 控件，用于选择 AtlasType 枚举类型。
    
    在 ComboBoxEdit.xaml 中添加了新的 ComboBoxEdit 控件的 XAML 定义，包括 DockPanel 布局和 ComboBox 控件。在 ComboBoxEdit.xaml.cs 中添加了 ComboBoxEdit 控件的代码逻辑，包括依赖属性的定义和 UpdateActualItemsSource 方法。

---
+++ MSDFAtlasGenerator/Controls/ComboBoxEdit.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using MSDFAtlasGenerator.Contracts;
+using IEnumerable = System.Collections.IEnumerable;
+
+namespace MSDFAtlasGenerator.Controls;
+
+/// <summary>
+/// ComboBoxEdit.xaml 的交互逻辑
+/// </summary>
+public partial class ComboBoxEdit : Edit
+{
+    public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register(nameof(ItemsSource),
+                                                                                                typeof(IEnumerable),
+                                                                                                typeof(ComboBoxEdit),
+                                                                                                new PropertyMetadata(null, (a, _) => ((ComboBoxEdit)a).UpdateActualItemsSource()));
+
+    public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register(nameof(SelectedItem),
+                                                                                                 typeof(object),
+                                                                                                 typeof(ComboBoxEdit),
+                                                                                                 new PropertyMetadata(null));
+
+    public static readonly DependencyProperty IsEnumProperty = DependencyProperty.Register(nameof(IsEnum),
+                                                                                           typeof(bool),
+                                                                                           typeof(ComboBoxEdit),
+                                                                                           new PropertyMetadata(false, (a, _) => ((ComboBoxEdit)a).UpdateActualItemsSource()));
+
+    public static readonly DependencyProperty EnumTypeProperty = DependencyProperty.Register(nameof(EnumType),
+                                                                                             typeof(Type),
+                                                                                             typeof(ComboBoxEdit),
+                                                                                             new PropertyMetadata(null, (a, _) => ((ComboBoxEdit)a).UpdateActualItemsSource()));
+
+    public static readonly DependencyProperty ActualItemsSourceProperty = DependencyProperty.Register(nameof(ActualItemsSource),
+                                                                                                      typeof(IEnumerable),
+                                                                                                      typeof(ComboBoxEdit),
+                                                                                                      new PropertyMetadata(null));
+
+    public ComboBoxEdit()
+    {
+        InitializeComponent();
+    }
+
+    public IEnumerable ItemsSource
+    {
+        get => (IEnumerable)GetValue(ItemsSourceProperty);
+        set => SetValue(ItemsSourceProperty, value);
+    }
+
+    public object SelectedItem
+    {
+        get => GetValue(SelectedItemProperty);
+        set => SetValue(SelectedItemProperty, value);
+    }
+
+    public bool IsEnum
+    {
+        get => (bool)GetValue(IsEnumProperty);
+        set => SetValue(IsEnumProperty, value);
+    }
+
+    public Type EnumType
+    {
+        get => (Type)GetValue(EnumTypeProperty);
+        set => SetValue(EnumTypeProperty, value);
+    }
+
+    public IEnumerable ActualItemsSource
+    {
+        get => (IEnumerable)GetValue(ActualItemsSourceProperty);
+        private set => SetValue(ActualItemsSourceProperty, value);
+    }
+
+    private void UpdateActualItemsSource()
+    {
+        ActualItemsSource = IsEnum && EnumType != null ? Enum.GetValues(EnumType) : ItemsSource;
+    }
+}
