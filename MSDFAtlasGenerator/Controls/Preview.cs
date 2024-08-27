fd0e79189d29a90cb01d4b4698741a9ab0373666
王先生 <1324771795@qq.com>
2024-08-27 11:14:23 +0800

    重构 Preview 类以使用 flipY 字段并优化渲染
    将 Preview 类中的 ImageData.FlipY 替换为新的私有布尔字段 flipY。在 OnRender 方法中，移除了对 ImageData.FlipY 的检查，改为检查 flipY 字段，并调整了 ImageBrush 的 Stretch 属性为 Stretch.Uniform。此外，添加了对 ActualWidth 和 ActualHeight 的计算，并使用这些值来绘制矩形。在 UpdatePreview 方法中，添加了对 flipY 字段的赋值，将其设置为 ImageData.FlipY 的值。

--- MSDFAtlasGenerator/Controls/Preview.cs
+++ MSDFAtlasGenerator/Controls/Preview.cs
@@ -20,6 +20,7 @@ public class Preview : Control
     }
 
     private WriteableBitmap? previewImage;
+    private bool flipY;
 
     protected override void OnRender(DrawingContext drawingContext)
     {
@@ -28,28 +29,29 @@ public class Preview : Control
             return;
         }
 
+        double width = ActualWidth;
+        double height = ActualHeight;
+
         ImageBrush imageBrush = new(previewImage)
         {
             AlignmentX = AlignmentX.Center,
             AlignmentY = AlignmentY.Center,
-            Stretch = Stretch.None
+            Stretch = Stretch.Uniform
         };
 
-        if (ImageData.Width > ActualWidth || ImageData.Height > ActualHeight)
+        if (flipY)
         {
-            imageBrush.Stretch = Stretch.Uniform;
-        }
+            drawingContext.PushTransform(new TranslateTransform(0.0, height));
+            drawingContext.PushTransform(new ScaleTransform(1.0, -1.0));
 
-        if (ImageData.FlipY)
-        {
-            drawingContext.PushTransform(new ScaleTransform(1, -1, ActualWidth / 2.0, ActualHeight / 2.0));
-        }
+            drawingContext.DrawRectangle(imageBrush, null, new Rect(0.0, 0.0, width, height));
 
-        drawingContext.DrawRectangle(imageBrush, null, new Rect(0, 0, ActualWidth, ActualHeight));
-
-        if (ImageData.FlipY)
-        {
             drawingContext.Pop();
+            drawingContext.Pop();
+        }
+        else
+        {
+            drawingContext.DrawRectangle(imageBrush, null, new Rect(0.0, 0.0, width, height));
         }
     }
 
@@ -67,6 +69,8 @@ public class Preview : Control
             }
 
             previewImage.WritePixels(new Int32Rect(0, 0, ImageData.Width, ImageData.Height), ImageData.Bgra, ImageData.Width * 4, 0);
+
+            flipY = ImageData.FlipY;
 
             InvalidateVisual();
         }
